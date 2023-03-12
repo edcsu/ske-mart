da79897dc3505f64ace2e0ec0e673c610cf52319
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-03-12 17:08:38 +0300

    refactor: lazy load manager module

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -2,12 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ManagerRoutingModule } from './manager-routing.module';
-import { Routes } from '@angular/router';
-import { ManagerHomeComponent } from './manager-home/manager-home.component';
-
-export const managerModuleRoutes: Routes = [
-  { path: '', component: ManagerHomeComponent }
-]
 
 @NgModule({
   declarations: [],
